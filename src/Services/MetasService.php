4e962757346721fdb755bfa9650ab440093a2ec6
Romain Chérot <romain.cherot@free.fr>
2023-11-09 19:55:10 +0100

    Ajout des variables dans les metas

--- src/Services/MetasService.php
+++ src/Services/MetasService.php
@@ -29,40 +29,50 @@ class MetasService {
     }
 
 
-    public static function getTitle($force_routename = false): string {
+    public static function getTitle(array $dynamic_vars_array = [], $force_routename = false): string {
 
         $key = self::getRouteName();
         $metas = self::loadMetasYaml();
+        $title = self::getDefaultTitle();
 
-        if (!empty($force_route)) {
-            $key = $force_route;
+        if (!empty($force_routename)) {
+            $key = $force_routename;
         }
 
-        // S'il y a un signe + un title spécifique, on retourne le #signe remplacé
         if (isset($metas->$key->title)) {
-            return $metas->$key->title;
+            $title = $metas->$key->title;
+        }
+
+        // Remplacement des variables de type {{ $post->title }}
+        if (count($dynamic_vars_array) > 0) {
+            $title = self::replaceDynamicContent($title, $dynamic_vars_array);
         }
 
         // Title par défaut
-        return self::getDefaultTitle();
+        return $title;
     }
 
-    public static function getDescription($force_routename = false): string {
+    public static function getDescription(array $dynamic_vars_array = [], $force_routename = false): string {
 
         $key = self::getRouteName();
         $metas = self::loadMetasYaml();
+        $description = self::getDefaultDescription();
 
-        if (!empty($force_route)) {
-            $key = $force_route;
+        if (!empty($force_routename)) {
+            $key = $force_routename;
         }
 
-        // S'il y a un signe + un title spécifique, on retourne le #signe remplacé
         if (isset($metas->$key->description)) {
-            return $metas->$key->description;
+            $description = $metas->$key->description;
+        }
+
+        // Remplacement des variables de type {{ $post->title }}
+        if (count($dynamic_vars_array) > 0) {
+            $description = self::replaceDynamicContent($description, $dynamic_vars_array);
         }
 
         // Title par défaut
-        return self::getDefaultDescription();
+        return $description;
     }
 
     public static function getDefaultTitle() {
@@ -82,5 +92,22 @@ class MetasService {
             throw new \ErrorException('\JoliMardi\MetasService : /config/metas.yml : default description introuvable (ajouter un item dans le yaml : [default: [title:"titre par défaut", description: "description par défaut]]');
             return '';
         }
+    }
+
+    // Les variables doivent êtres passées en tableau comme pour les views, avec compact();
+    public static function replaceDynamicContent($string, $array_of_data_vars) {
+        if (count($array_of_data_vars) > 0) {
+            $string = preg_replace_callback('/{{ (.*?) }}/', function ($matches) use ($array_of_data_vars) {
+                extract($array_of_data_vars);
+                $placeholder = $matches[0];
+                $new_value = false;
+
+                // On n'affiche pas les erreurs
+                @eval('$new_value = ' . $matches[1] . ';');
+
+                return $new_value ?? $placeholder;
+            }, $string);
+        }
+        return $string;
     }
 }
